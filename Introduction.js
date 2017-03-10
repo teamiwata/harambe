290d8c1f9b3ccb79578a995ea11969c77cb0c6f2
HelcoMarc <marcbrian15@outlook.com>
2017-03-10 10:15:26 -0500

    Add files via upload
    Added Introduction

---
+++ Introduction.js
@@ -0,0 +1,12 @@
+document.getElementById("Introduction").style.width = "590px";
+document.getElementById("Introduction").style.height = "590px";
+document.getElementById("Introduction").style.border = "thick solid #000000";
+document.getElementById("Introduction").style.margin = "50px 50px 50px 50px";
+document.getElementById("Introduction").style.backgroundColor = "#A4A4A4";
+
+document.getElementById("Story").style.fontSize = "x-large";
+document.getElementById("Story").style.color = "Red";
+
+document.getElementById("closeText").style.position = "relative";
+document.getElementById("closeText").style.cssFloat = "right";
+document.getElementById("closeText").style.bottom = "50px";
